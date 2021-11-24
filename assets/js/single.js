3d691009a1ad715bbc10ed514155b9b649af6c43
mikwitz <mikaylahowitz@gmail.com>
2021-11-24 14:27:51 -0700

    successfully links between pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,8 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var repoNameEl = document.querySelector("#repo-name")
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function(response) {
@@ -12,9 +14,21 @@ var getRepoIssues = function(repo) {
                 }
             })
         } else {
-            alert("There was a problem with your request!")
+            document.location.replace("./index.html");
         }
     });
+}
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+
+    } else {
+        document.location.replace("./index.html");
+    }
 }
 
 var displayIssues = function(issues) {
@@ -58,4 +72,4 @@ var displayWarning = function(repo) {
 
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
